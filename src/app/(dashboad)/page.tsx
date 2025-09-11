b63f6dc6d70504e6777cdbb11e36681d2b1c112d
SprigganTwelve <angepaterne.dali@gmail.com>
2025-09-11 21:17:53 +0200

    dashboard layout gloom animation on details sections - done

--- src/app/(dashboad)/page.tsx
+++ src/app/(dashboad)/page.tsx
@@ -1,15 +1,45 @@
+"use client";
 
 import clsx from "clsx";
-import Image from "next/image";
 import styles from './dashboard.module.css'
 import RoundStep from "@/ui/roundStep/RoundStep";
+import { useEffect, useRef } from "react";
 
 
 export default function Home() {
+
+
+  const cardRefs = useRef<HTMLDivElement[]>([]);
+
+  const backlights = useRef([
+    "purple",
+    "rgba(129, 25, 25, 0.356)",
+    "rgba(179, 114, 114, 0.945)",
+    "rgba(111, 57, 212, 0.904)"
+  ]);
+
+  useEffect(() => {
+      cardRefs.current.forEach((card, i) => {
+        card.onmousemove = (e)=>{
+          let x = e.pageX - card.offsetLeft;
+          let y = e.pageY- card.offsetTop;
+          card.style.setProperty('--x', x + 'px');
+          card.style.setProperty('--y', y + 'px');
+        }
+      });
+  }, []);
+
+
+  const setCardRef = (index: number) => (el: HTMLDivElement | null) => {
+    if (el) cardRefs.current[index] = el;
+  };
+
   return (
     <div className={styles.container}>
       
-        <div className="glassEffect curves rounded-[20px]" >
+        <div
+            className="glassEffect curves"
+        >
               
         </div>
 
@@ -17,11 +47,17 @@ export default function Home() {
 
         </div>
 
-        <div className={styles.detailContainer}>
-          <RoundStep />
-          <RoundStep />
-          <RoundStep />
-          <RoundStep />
+        <div id="details" className={styles.detailContainer}>
+          {
+            backlights.current.map((color, i )=>(
+              <RoundStep 
+                  key={i}
+                  setRef={setCardRef(i)}
+                  className={styles.card} 
+                  style={{["--backlight" as any] : color }}
+              />
+            ))
+          }
         </div>
 
         <div className="flex justify-between gap-4">
--- src/ui/roundStep/RoundStep.tsx
+++ src/ui/roundStep/RoundStep.tsx
@@ -1,17 +1,37 @@
 import clsx from "clsx";
 import styles from '@/ui/roundStep/roundStep.module.css'
 
-const RoundStep = () => {
-    return ( 
-          <div className={clsx(styles.container, "glassEffect")}>
-                <div>
 
-                </div>
-                <div>
-                    
-                </div>
-          </div>
-     );
+
+
+interface RoundedStepProps {
+      id?: string;
+      className?: string;
+      style?: React.CSSProperties;
+      setRef?: (el: HTMLDivElement | null) => void;
 }
+
+
+
+const RoundStep: React.FC<RoundedStepProps> = ({
+      id,
+      style,
+      className,
+      setRef,
+}) => {
+  return (
+      <div
+            id={id}
+            ref={setRef}
+            style={style}
+            className={clsx(styles.container, "glassEffect", className)}
+      >
+            <div></div>
+            <div></div>
+      </div>
+  );
+};
+
+
  
 export default RoundStep;
